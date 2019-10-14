acb2495a33f5b87a665c04c07bd498406e9b3cfc
Maurice Webb <mwebb.mw@gmail.com>
2019-10-13 22:32:53 -0400

    work in progress

--- routes /apiRoutes.js
+++ routes /apiRoutes.js
@@ -1,9 +1,8 @@
 var express = require('express');
-
-var router = express.Router();
+var router = express.apiRouter();
 
 router.get('/', function(req,res){
     res.render('index');
 });
 
-module.exports = router; 
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-var http = require('express');
-
+var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 3002;
-//app.get('/', function(req, res){
-  //res.send('hello world');
-//});
+
+//app.use(express.static(__dirname + '/public'));
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8081;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//var routes = require('/routes/apiRoutes.js');
+//app.use('/', apiRoutes);
+//require("./routes/apiRoutes")(app);
+//require("./routes/htmlRoutes")(app);
+
+
 app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
-
-var routes = require('./routes/apiRoutes.js');
-app.use('/',routes);
-
